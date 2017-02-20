7a31503f1a0f5d8f12e6930c1a79fbfc1bd9285e
M̰͒o̳̲̭͓̗͈̍͟n͐̄ͪͥͧk̉ <sabintheworld@gmail.com>
2017-02-20 00:25:58 -0800

    Auth Stuff

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ MongoClient.connect('mongodb://root:798140sa@ds153239.mlab.com:53239/nodexpress'
 });
 
 //Services
-var BadgeService = require('./services/badgeService');
 var AuthService = require('./services/authService');
 
-//Tells the server to parse out URL encoded form data
-app.use(bodyParser.urlencoded({extended: true}))
+app.use( bodyParser.json() ); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({extended: true})) //Tells the server to parse out URL encoded form data
 
 //Middleware to check for bearer token and authenticate/validate user
 app.use((req, res, next) => {
@@ -27,18 +26,19 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
 
   if(req.method == 'OPTIONS'){
-  	res.status(200).send({message: "Preflight check successful"});
+  	return res.status(200).send({message: "Preflight check successful"});
   }
-  
+  if(req.url == '/login'){
+  	return next();
+  }
   if(req.headers.authorization){
   	let bearer = req.headers.authorization.split(`Bearer `)[1];
 	_authService.verifyToken(bearer, (data) => {
-		if(data) next();
-		else res.status(401).send({message: "Not Authenticated"});
+		if(data) return next();
+		else return res.status(401).send({message: "Not Authenticated"});
 	});
   } else {
-	res.status(401).send({message: "Not Authenticated."})
-	next();
+	return res.status(401).send({message: "Not Authenticated."})
   }
 })
 
@@ -52,63 +52,17 @@ app.set('view engine', 'pug');
 //
 // ***************************************************************************************
 
-	app.get('/', (req, res) => {
-		res.render('index', {title:"Badge Site"});
-	})
-
-	//The homepage that shows all the badges
-	app.get('/home', (req, res) => {
-		let _badgeService = new BadgeService(db);
-		_badgeService.getBadges((data) => {
-			res.render('home', {title: 'Home', message: 'List of badges!', badges: data});
-		});
-	});
-
-	//Renders the new badge page
-	app.get('/manageBadges', (req, res) => {
-		res.render('manageBadges', { title: 'Manage Badges'});
-	});
-
-
-// ***************************************************************************************
-//
-// Badge Stuff
-//
-// ***************************************************************************************
-
-
-
-//Grabs all the badges
-app.get('/badges', (req, res) => {
-	let _badgeService = new BadgeService(db);
-	_badgeService.getBadges((data) => {
-		res.send(data);
-	});
-});
+// app.get('/', (req, res) => {
+// 	res.render('index', {title:"Badge Site"});
+// });
 
 //Grabs a badge with a given ID
-app.get('/badge/:id', (req, res) => {
-	let _badgeService = new BadgeService(db);
-	_badgeService.getBadgeByID(+req.params.id, (data) => {
-		res.send(data);
-	});
-});
-
-//Creates a new Badge
-app.post('/newbadge', (req, res) => {
-	let _badgeService = new BadgeService(db);
-	_badgeService.saveBadge(req.body, (data) => {
-		data ? res.send(data) : res.status(401).send({error: "There was a problem with this request."});
-	});
-});
-
-//Deletes a badge
-app.post('/deletebadge', (req, res) => {
-	let _badgeService = new BadgeService(db);
-	_badgeService.deleteBadge(+req.body.id, (data) => {
-		data ? res.status(200).send({success: "Successfully deleted badge."}) : res.status(401).send({error: "There was a problem with this request."});
-	});
-})
+// app.get('/badge/:id', (req, res) => {
+// 	let _badgeService = new BadgeService(db);
+// 	_badgeService.getBadgeByID(+req.params.id, (data) => {
+// 		res.send(data);
+// 	});
+// });
 
 
 // ***************************************************************************************
@@ -117,12 +71,13 @@ app.post('/deletebadge', (req, res) => {
 //
 // ***************************************************************************************
 
-
-
-// POST method route
-app.post('/', (req, res) => {
-  res.send('POST request to the homepage')
+app.post('/login', (req, res) => {
+	let _authService = new AuthService(db);
+	_authService.login(req.body, data => {
+		return res.send( data != false ? {status: 200, user: data} : {status: 401, error: 'Invalid Login'});
+	});
 });
+
 
 
 app.listen(3000, () => {
--- services/authService.js
+++ services/authService.js
@@ -1,14 +1,44 @@
+var crypto = require('crypto');
+
 module.exports = class AuthService {
 	constructor(db){
 		this.db = db;
 	}
-
+	//Creates randomized UUID Token for authentication
+	_createUUID() {
+		var d = new Date().getTime();
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace( /[xy]/g, function( c ) {
+			let r = ( d + Math.random() * 16 ) % 16 | 0;
+			d = Math.floor( d / 16 );
+			return( c == 'x' ? r : ( r & 0x7 | 0x8 ) ).toString( 16 );
+		});
+	}
 
 	verifyToken(token, cb) {
 		this.db.collection('users').find( { device_token: token.toString() } ).toArray((err, data) => {
 			// Here we could set the user's token to something else and then return the new user to the client
 			// so they'd be getting a new token each request. This would make it literally impossible to crack.
 			cb(data.length ? true : false);
+		});
+	}
+
+
+	login(data, cb) {
+		let password_hash = crypto.createHash('md5').update(data.password).digest('hex');
+		this.db.collection('users').find( { password: password_hash, username: data.username } ).toArray((err, retrieve_data) => {
+			if(retrieve_data.length == 1){
+				let token = this._createUUID();
+				this.db.collection('users').update({password: password_hash, username: retrieve_data[0].username}, {$set : {token: token}}, (err, update_data) => {
+					cb({
+						username: retrieve_data[0].username,
+						email: retrieve_data[0].email,
+						remember_me: retrieve_data[0].remember_me,
+						token: token
+					});
+				});
+			} else {
+				cb(false);
+			}
 		});
 	}
 }
